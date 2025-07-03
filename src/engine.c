5e72d150ddfb2122e6f89a0c26ee51a8500c4e11
Adharsh Shokkalingam <Harsh.shokka@gmail.com>
2025-07-03 16:14:55 -0600

    reading the files in the assests folder

--- src/engine.c
+++ src/engine.c
@@ -2,6 +2,20 @@
 #include "engine.h"
 
 void list_asset_files(const char* folder){
-    DIR *dir = opendir()
+    DIR *dir = opendir(folder); 
 
+    if(!dir){
+        printf("Unable to oepn assets directory!\n"); 
+        return 0; 
+    }
+
+    printf("Avaliable object files: \n"); 
+    struct dirent *entry;
+
+    while(entry == readdir(dir) != NULL){
+        if(strstr(entry->d_name, ".obj"));
+        printf("- %s\n", entry->d_name);
+    }
+
+    closedir(dir); 
 }
